8ceceb14e02939151dfaca4d3849cf82af3dc638
alma-hernandez <1984ahern@gmail.com>
2023-12-02 14:16:12 -0600

    added delete and mark_complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
         render 'tasks/index'
     end
     def create
+
         @task = Task.new(task_params)
         if @task.save
           render 'tasks/create' # can be omitted
@@ -13,4 +14,23 @@ class TasksController < ApplicationController
         def task_params
           params.require(:task).permit(:content)
         end
+
+    def destroy
+        @task = Task.find_by(id: params[:id])
+        
+        if @task and @task.destroy
+            render json: { success: true }
+        else
+            render json: { success: false }
+        end
+     end
+        private
+
+    def mark_complete
+        @task = Task.find_by(id: params[:id])
+        if @task and @task.update(completed: true)
+            render 'tasks/mark_complete'
+        end
+    end
+          private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   # Add routes below this line
   get 'tasks' => 'tasks#index'
+
   # Add routes below above line
   post 'tasks' => 'tasks#create'
-
+  
+  delete 'task/:id' => 'tasks#destroy'
+  
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
+  put '/tasks/:id/mark/complete' => 'tasks#mark_complete'
 end
